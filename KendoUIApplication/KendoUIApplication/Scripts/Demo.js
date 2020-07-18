a69c17441d47f5f0b714457447c86488972d238b
Sachin Shelke <Sachin.Shelke@IDeaS.com>
2020-07-18 22:42:36 +0530

    Added new project

---
+++ KendoUIApplication/KendoUIApplication/Scripts/Demo.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="kendo/2014.1.318/jquery.min.js" />
+/// <reference path="kendo/2014.1.318/kendo.web.min.js" />
+
+$(document).ready((function () {
+
+    var CountryList = [
+        "America",
+        "Australia",
+        "China",
+        "India",
+        "Pakistan"
+    ];
+
+    $("#Country").kendoAutoComplete({
+        dataSource: CountryList,
+        filter: "startswith",
+        placeholder: "Select Country",
+        seperator: ","
+
+    });
+
+}));
